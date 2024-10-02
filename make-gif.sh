1ff809bc98c61ebebd3f4c1b69e4c84e636792e3
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2024-10-02 15:04:53 +0300

    screenshots

--- make-gif.sh
+++ make-gif.sh
@@ -2,10 +2,11 @@ set -ex
 rm -f screenshot/*.png
 cargo run --bin take_screenshots_and_exit
 cd screenshot
-ffmpeg  -y -i '%05d.png' -filter_complex "fps=50,scale=1024:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=32[p];[s1][p]paletteuse=dither=bayer" output.gif
+ffmpeg  -y -i '%05d.png' -filter_complex "fps=50,scale=768:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=32[p];[s1][p]paletteuse=dither=bayer" output.gif
 
 ffmpeg  -y -i '%05d.png' -filter_complex "fps=50,scale=1024:-1:flags=lanczos,split[s0][s1];[s0]palettegen=max_colors=32[p];[s1][p]paletteuse=dither=bayer" -c:v libx264 -profile:v baseline -level 3.0 -pix_fmt yuv420p  output.mp4
-cp screenshot0.png tmp
+cp 00001.png tmp
 rm -f *.png
 mv tmp output.png
+git add -f output.*
 ls -alh output.gif
--- readme.md
+++ readme.md
@@ -1,9 +1,3 @@
-- download/isntall clang++
-    - https://github.com/llvm/llvm-project/releases
-    - install "LLVM-$VERSION-Windows-X64.tar.xz" (1 GB)
-    - envvars: set "PATH" to /bin and "C_INCLUDE_PATH", "CPLUS_INCLUDE_PATH" to /include
-
-wasm / ibin 150
-wasm/malachite 138
-wasm/dashu 180
-wasm/rug 
+![](./screenshot/output.mp4)
+![](./screenshot/output.gif)
+![](./screenshot/output.png)
